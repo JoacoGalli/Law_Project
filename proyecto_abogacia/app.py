59c18e9b9016decb8f07e1a20ce66f9b0f829495
JoacoGalli <gallijoaquin@protonmail.com>
2021-11-11 14:07:05 -0300

    agrego create index y login

--- proyecto_abogacia/app.py
+++ proyecto_abogacia/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, flash, url_for
 from flaskext.mysql import MySQL
+import numpy as np
 
 app = Flask(__name__)
+app.secret_key="Develoteca"
 
 mysql = MySQL ()
 app.config['MYSQL_DATABASE_HOST']='localhost'
@@ -12,12 +14,16 @@ mysql.init_app(app)
 
 @app.route('/')
 def  index():
-    #sql = "INSERT INTO `usuarios` (`usuario`, `contraseña`) VALUES ('pepito', 'nodebora');"
-    #conn=mysql.connect()
-    #cursor=conn.cursor()
-    #cursor.execute(sql)
-    #conn.commit()
-    return render_template('usuarios/index.html')
+    sql = "SELECT * FROM `usuarios`;"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    
+    usuarios = cursor.fetchall() #selecciona todos los datos
+    print(usuarios)
+
+    conn.commit()
+    return render_template('usuarios/index.html', usuarios=usuarios)
 
 @app.route('/create')
 def create():
@@ -27,15 +33,25 @@ def create():
 def storage():
     _usuario=request.form['txtUsuario']
     _contraseña=request.form['txtContraseña']
-    sql = "INSERT INTO `usuarios` (`usuario`, `contraseña`) VALUES (%s, %s);"
-    
-    datos = (_usuario,_contraseña)
 
+    #sql = "INSERT INTO `usuarios` (`usuario`, `contraseña`) VALUES (%s, %s);"
+    
+    #datos = (_usuario,_contraseña) 
+    sql = "SELECT * FROM `usuarios`;"
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute(sql,datos)
-    conn.commit()
-    return render_template('usuarios/index.html')
+    cursor.execute(sql)
+    usuarios = cursor.fetchall()
+    for i in usuarios:
+        print(i)
+        print(i[0])
+        if _usuario==i[0] and _contraseña==i[1]:
+            conn.commit()
+            return render_template('usuarios/login.html')
+        else:
+            continue
+    flash('Usuario incorrecto')
+    return redirect(url_for('create'))
 
 if __name__ == '__main__':
     app.run(debug=True)
